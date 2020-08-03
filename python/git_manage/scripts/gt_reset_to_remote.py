bbf9e97cfcbbe7bca458b014d54e0bc4c34aca20
Dan Aukes <danaukes@gmail.com>
2020-08-03 10:24:05 -0700

    removed old reset code

--- python/git_manage/scripts/gt_reset_to_remote.py
+++ python/git_manage/scripts/gt_reset_to_remote.py
@@ -78,23 +78,3 @@ if __name__=='__main__':
     git_list = git_tools.find_repos(p1,search_depth = 5,exclude=exclude)
 
     reset_branches(git_list)
-
-#     not_synced = []
-#     for item in git_list:
-#         print(item)
-#         r = Repo(item)
-#     #    for b in r.branches:
-#         b = r.active_branch
-#         rem = b.tracking_branch()
-#         if b.commit.hexsha != rem.commit.hexsha:
-#             if not r.is_dirty(untracked_files=True):
-#                 if r.is_ancestor(b.commit,rem.commit):
-#                     print('Yes')
-# #                    r.head.reset(rem.commit)
-#                     r.head.reset(rem.commit,index=True,working_tree=True)
-#                     for item2 in r.untracked_files:
-#                         os.remove(os.path.join(item,item2))
-                    
-
-
-
