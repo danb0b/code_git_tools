30bbb77d7ee4acbdf71ef2399f9c33d0551ed03d
Dan Aukes <danaukes@gmail.com>
2021-06-23 10:03:22 -0700

    updated

--- add_to_path.sh
+++ add_to_path.sh
@@ -2,5 +2,6 @@
 
 echo "export PATH=\$PATH:$PWD/bash" >> ~/.bashrc
 echo "export PATH=\$PATH:$PWD/python/git_manage" >> ~/.bashrc
+chmod +x python/git_manage/gitman.py
 chmod +x *.sh
 chmod +x *.bash
